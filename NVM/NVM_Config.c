d9717f3c460c98f133a0f7bb01a702708acafb05
wessamKoraim <wessamadel1996@gmail.com>
2018-06-27 15:51:04 +0200

    Non-Volatile Memory Manager addition

---
+++ NVM/NVM_Config.c
@@ -0,0 +1,13 @@
+#include "NVM_Manager.h"
+#include "NVM_Config.h"
+
+NVM_ConfigType NVM_ConfigParam [NO_OF_NVM_USED] = 
+{
+    {
+        0xA0,//Slave Address
+        4, //Numer of Bytes
+        0x30, //Location Address to Read Or Write
+        NVM_WriteDone,//Write call back function
+        NVM_ReadDone //Read call back function
+    }
+};
---
+++ NVM/NVM_Manager.h
@@ -0,0 +1,51 @@
+#ifndef NVM_MANAGER_H
+#define NVM_MANAGER_H
+#include <stdint.h>
+#include "I2C_Manager.h"
+#include "NVM_Config.h"
+
+
+typedef void (*NVM_CallBackPointerType)(void);
+
+void NVM_WriteDone(void);
+void NVM_ReadDone(void);
+
+
+typedef enum
+{
+    NVM_UN_INIT,
+    NVM_IDLE,
+    NVM_WRITE,
+    NVM_READ,
+    NVM_WAIT
+}NVM_States;
+
+typedef enum 
+{
+    NVM_BUSY,
+    NVM_NOT_BUSY,
+    NVM_OK,
+    NVM_NOK
+}NVM_CheckType;
+
+typedef struct
+{
+    uint8_t NVM_SlaveAddress;
+    uint8_t NVM_NoOfBytes;
+    uint8_t NVM_LocationAddress;
+    NVM_CallBackPointerType NVM_WriteDoneCallBackPtr;
+    NVM_CallBackPointerType NVM_ReadDoneCallBackPtr;
+
+}NVM_ConfigType;
+
+
+
+NVM_CheckType NVM_Init(void);
+NVM_CheckType NVM_Write(uint8_t ConfigStructure_ID, uint8_t* DataPointer);
+NVM_CheckType NVM_Read(uint8_t ConfigStruct, uint8_t* DataPointer);
+void NVM_Manager(void);
+
+extern uint8_t I2C_InitFlag ;
+extern NVM_ConfigType NVM_ConfigParam [NO_OF_NVM_USED];
+#endif
+
